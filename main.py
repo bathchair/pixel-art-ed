b3085cb50f3a206b558580f9740d14b7fca54da5
Dea Harjianto <64983004+bathchair@users.noreply.github.com>
2021-07-07 12:14:07 -0400

    feat: create grid of cells

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ class PixelArt:
         self.root = root
         self.root.title("Pixel Art Editor")
 
+        cell_length = 50
+        grid_width = 20
+        grid_height = 10
+
+        self.drawing_grid = Canvas(self.root)
+        self.drawing_grid.grid(column=0, row=0, sticky=(N,E,S,W))
+
+        self.cells = []
+        for i in range(grid_width):
+            for j in range(grid_height):
+                cell = Frame(self.drawing_grid, width=cell_length, height=cell_length, bg="white", highlightbackground="black", highlightcolor="black", highlightthickness=1)
+                cell.grid(column=i, row=j)
+                self.cells.append(cell)
+
+
+
 root = Tk()
 PixelArt(root)
 root.mainloop()
